2b92ba423e293c046342f42e2688ce0a747de0a9
RomanBelkov <romanbelkov@gmail.com>
2014-04-03 21:59:37 +0400

    Done 2 tasks from H/W 2

---
+++ Task №2/Project Euler/10/Program.fs
@@ -0,0 +1,18 @@
+﻿    open System.Numerics
+
+    let isPrime (n : BigInteger) =
+        match n with
+        | _ when n > 3I && (n % 2I = 0I || n % 3I = 0I) -> false
+        | _ ->
+            let upperLimit = BigInteger(System.Math.Sqrt(float n)) + 1I
+            let rec loop (d : BigInteger) (i : BigInteger) = 
+                if d > upperLimit then 
+                    true
+                else
+                    if n % d = 0I then 
+                        false
+                    else
+                        loop (d + i) i    
+            loop 5I 2I
+    
+    let result = [2I..1999999I] |> List.filter (fun x -> isPrime x) |> List.sum
