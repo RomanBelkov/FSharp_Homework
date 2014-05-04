a5f7667c72d4445cce2edc754aee38ea3f644702
RomanBelkov <romanbelkov@gmail.com>
2014-05-04 13:01:25 +0400

    Fixed "Optimization" program

--- Task №2/Optimization/Program.fs
+++ Task №2/Optimization/Program.fs
@@ -7,52 +7,41 @@
     | Div of Expr * Expr
 
 let rec optim expr =
+    let simplification = function
+        | Add (Const x, Const y) -> Const (x + y)
+        | Add (x, Const 0) | Add (Const 0, x) ->  x
+
+        | Sub (Const x, Const y) -> Const (x - y)
+        | Sub (x, Const 0) | Sub (Const 0, x) ->  x
+        | Sub (Var x, Var y) ->
+            if (x = y) then
+                Const 0
+            else
+                Sub (Var x, Var y)
+
+        | Mul (Const x, Const y) -> Const (x + y)
+        | Mul (x, Const 0) | Mul (Const 0, x) -> Const 0
+        | Mul (x, Const 1) | Mul (Const 1, x) ->  x
+
+        | Div (Const x, Const y) -> Const (x / y)
+        | Div (x, Const 1) ->  x
+        | Div (Var x, Var y) ->
+            if x = y then
+                Const 1
+            else
+                Div (Var x, Var y)
+        | Div (x, Const 0) -> failwith "ERROR! Division by zero is strictly prohibited"
+        | expression -> expression 
+
     match expr with
-    | Const x -> Const x
-    | Var s -> Var s
-    
-    | Add (Const x, Const y) -> Const (x + y)
-    | Add (x, Const 0) -> optim x
-    | Add (Const 0, x) -> optim x
-    | Add (x, y) ->
-        let a = optim x
-        let b = optim y
-        Add (a,b)
+    | Add(a, b) -> simplification(Add(optim a, optim b))
+    | Sub(a, b) -> simplification(Sub(optim a, optim b))
+    | Mul(a, b) -> simplification(Mul(optim a, optim b))
+    | Div(a, b) -> simplification(Div(optim a, optim b))
+    | Const a -> expr
+    | Var a -> expr
 
-    | Sub (Const x, Const y) -> Const (x - y)
-    | Sub (x, Const 0) -> optim x
-    | Sub (Const 0, x) -> optim x
-    | Sub (Var x, Var y) ->
-        if (x = y) then
-            Const 0
-        else
-            Sub (Var x, Var y)
-    | Sub (x,y) -> 
-        let a = optim x
-        let b = optim y
-        Sub (x, y)
-
-    | Mul (Const x, Const y) -> Const (x + y)
-    | Mul (x, Const 0) -> Const 0
-    | Mul (Const 0, x) -> Const 0
-    | Mul (x, Const 1) -> x
-    | Mul (Const 1, x) -> x
-    | Mul (x, y) ->
-        let a = optim x
-        let b = optim y
-        Mul (a, b)
-
-    | Div (Const x, Const y) -> Const (x / y)
-    | Div (x, Const 1) -> x
-    | Div (Var x, Var y) ->
-        if x = y then
-            Const 1
-        else
-            Div (Var x, Var y)
-    | Div (x, Const 0) -> failwith "ERROR! Division by zero is strictly prohibited"
-    | Div (x, y) ->
-        let a = optim x
-        let b = optim y
-        Div (a, b)
-
-printfn "%A" (optim (Div (Var "x", Const 0)))
+let first = Sub(Const 1, Const 1)
+let second = Sub(Const 2, Const 2)
+let e = Sub(first, second)
+printfn "%A" (optim e)
