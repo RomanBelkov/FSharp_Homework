c508eb1786a9f6a4671291c9760a64dcc45b8a1b
Roman Belkov <romanbelkov@gmail.com>
2014-11-05 13:20:08 +0300

    Added RegEx Task

---
+++ 3 term/RegEx_Task/RegEx_Task/Checker.fs
@@ -0,0 +1,15 @@
+﻿module Checker
+
+open System.Text.RegularExpressions
+
+let isMail src =
+    let pre = "[a-zA-Z+_][\w\-]*(\.[\-\w]+)*"
+    let post = "([\w\-]+[\.])+((\w{2,3}|info|museum|travel|name|mobi|jobs|coop|asia|aero|marketing|sales|support|abuse|security|postmaster|hostmaster|usenet|webmaster))"
+    let pattern = "^" + pre + "@" + post + "$" 
+    Regex.Match(src, pattern).Success
+
+//[<EntryPoint>]
+//let main argv = 
+//    let b = isMail "yo@domain.somedomain"
+//    printfn "%A" b
+//    0 
---
+++ 3 term/RegEx_Task/RegEx_Task/Tests.fs
@@ -0,0 +1,73 @@
+﻿module CheckTests
+
+open NUnit.Framework
+open FsUnit
+open Checker
+
+[<TestFixture>]
+module test = 
+    [<Test>]
+    let ``OK1`` () = 
+        let input = "a@b.cc"
+        let res = isMail input
+        res |> should equal true
+
+    [<Test>]
+    let ``OK2`` () = 
+        let input = "victor.polozov@mail.ru"
+        let res = isMail input
+        res |> should equal true
+
+    [<Test>]
+    let ``OK3`` () = 
+        let input = "vector.vek@gmail.com"
+        let res = isMail input
+        res |> should equal true
+
+    [<Test>]
+    let ``OK4`` () = 
+        let input = "my@domain.info"
+        let res = isMail input
+        res |> should equal true
+
+    [<Test>]
+    let ``OK5`` () = 
+        let input = "_.1@mail.com"
+        let res = isMail input
+        res |> should equal true
+
+    [<Test>]
+    let ``OK6`` () = 
+        let input = "paints_department@hermitage.museum"
+        let res = isMail input
+        res |> should equal true
+
+    [<Test>]
+    let ``NO1`` () = 
+        let input = "a@b.c"
+        let res = isMail input
+        res |> should equal false
+
+    [<Test>]
+    let ``NO2`` () = 
+        let input = "a..b@mail.ru"
+        let res = isMail input
+        res |> should equal false
+
+    [<Test>]
+    let ``NO3`` () = 
+        let input = ".a@mail.ru"
+        let res = isMail input
+        res |> should equal false
+
+    [<Test>]
+    let ``NO4`` () = 
+        let input = "yo@domain.somedomain"
+        let res = isMail input
+        res |> should equal false
+
+    [<Test>]
+    let ``NO5`` () = 
+        let input = "1@mail.ru"
+        let res = isMail input
+        res |> should equal false
